02f3d91952a06c8abc254a6f132c30b21c0ac53d
stephecloutier <stephecloutier@gmail.com>
2017-03-23 08:54:47 +0100

    Added game + player Model (to complete) + comment code

--- router.php
+++ router.php
@@ -15,9 +15,9 @@ if(!file_exists(INI_FILE) && !file_exists(SOURCE_NAME)) {
 require 'configs/routes.php';
 $default_route = $routes['default'];
 $route_parts = explode('/', $default_route);
-$method = $_SERVER['REQUEST_METHOD'];
-$a = $_REQUEST['a']??$route_parts[1];
-$r = $_REQUEST['r']??$route_parts[2];
+$method = $_SERVER['REQUEST_METHOD'];  // récupérer la Méthode
+$a = $_REQUEST['a']??$route_parts[1];  // récupérer l'Action
+$r = $_REQUEST['r']??$route_parts[2];  // récupérer la Ressource
 
 if (!in_array($method . '/' . $a . '/' . $r, $routes)) {
     die('ce que vous cherchez n’est pas ici');
